5a9135a0aaa01f8b317f660a1860cbf97cc3c1dc
Chuck Durfee <cdurfee@ghx.com>
2014-07-16 11:41:01 -0600

    spare case working

--- BowlingGame/ruby/game.rb
+++ BowlingGame/ruby/game.rb
@@ -1,11 +1,30 @@
+class Array
+  def sum
+    self.reduce(0, :+)
+  end
+end
+
 class Game
-  attr_reader :score
-  
   def initialize
-    @score = 0
+    @tries = []
   end
   
   def roll(pins)
-    @score += pins
+    @tries << pins
+  end
+  
+  def score
+    score = @tries.sum
+    
+    frames = @tries.each_slice(2).to_enum
+    loop do
+      f = frames.next
+      if f.sum == 10
+        spare_pin = frames.peek.first
+        score += spare_pin
+     end
+    end
+    
+    score
   end
 end
--- BowlingGame/ruby/spec/game_spec.rb
+++ BowlingGame/ruby/spec/game_spec.rb
@@ -8,6 +8,9 @@ class Game_spec
     end
     
     it "can score a gutter game" do
+      20.times do
+        @game.roll(0)
+      end
       expect(@game.score).to eq(0)
     end
     
@@ -16,6 +19,16 @@ class Game_spec
         @game.roll(1)
       end
       expect(@game.score).to eq(20)
+    end
+    
+    it "can score a spare" do
+      @game.roll(4)
+      @game.roll(6) #spare
+      @game.roll(3)
+      17.times do
+        @game.roll(0)
+      end
+      expect(@game.score).to eq(16)
     end
   end
 end
