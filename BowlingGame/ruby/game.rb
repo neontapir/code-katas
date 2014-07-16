ddaf04983d1362119e635824c9b592d30763dd99
Chuck Durfee <cdurfee@ghx.com>
2014-07-16 11:51:35 -0600

    strike case working

--- BowlingGame/ruby/game.rb
+++ BowlingGame/ruby/game.rb
@@ -1,6 +1,6 @@
 class Array
   def sum
-    self.reduce(0, :+)
+    self.reduce(0) { |sum, x| sum += x || 0 }
   end
 end
 
@@ -11,18 +11,24 @@ class Game
   
   def roll(pins)
     @tries << pins
+    @tries << nil if pins == 10
   end
   
   def score
     score = @tries.sum
     
     frames = @tries.each_slice(2).to_enum
+    #p frames
     loop do
       f = frames.next
-      if f.sum == 10
-        spare_pin = frames.peek.first
-        score += spare_pin
-     end
+      if f.first == 10
+        strike_bonus = frames.peek.sum
+        #p "Strike bonus: #{frames.peek}"
+        score += strike_bonus
+      elsif f.sum == 10
+        spare_bonus = frames.peek.first
+        score += spare_bonus
+      end
     end
     
     score
--- BowlingGame/ruby/spec/game_spec.rb
+++ BowlingGame/ruby/spec/game_spec.rb
@@ -22,13 +22,31 @@ class Game_spec
     end
     
     it "can score a spare" do
-      @game.roll(4)
-      @game.roll(6) #spare
+      roll_spare
       @game.roll(3)
       17.times do
         @game.roll(0)
       end
       expect(@game.score).to eq(16)
+    end
+    
+    it "can score a strike" do
+      roll_strike
+      @game.roll(6)
+      @game.roll(3)
+      17.times do
+        @game.roll(0)
+      end
+      expect(@game.score).to eq(28)
+    end
+    
+    def roll_spare
+      @game.roll(4)
+      @game.roll(6) #spare
+    end
+    
+    def roll_strike
+      @game.roll(10)
     end
   end
 end
