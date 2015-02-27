9ebc0e965e2843dba99c8a21c5b32164298ee0f5
Chuck Durfee <neontapir@gmail.com>
2015-02-27 12:35:58 -0700

    an attempt to refactor into frames

--- BowlingGame/csharp-functional/BowlingGame/BowlingGame.Core/Game.cs
+++ BowlingGame/csharp-functional/BowlingGame/BowlingGame.Core/Game.cs
@@ -3,27 +3,61 @@ using NUnit.Framework;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
+using System.CodeDom.Compiler;
+using NUnit.Framework.Constraints;
+using System.Runtime.Versioning;
 
 namespace Sandbox
 {
 	public class Game
 	{
-		List<int> results = new List<int>();
+		List<Frame> results = new List<Frame>();
 
-		public void Roll(int pins) {
-			results.Add(pins);
+		public void Roll(params int[] pins) {
+			var frame = new Frame(pins);
+			results.Add(frame);
 		}
 
 		public int Score() {
-			return results.EndGame().ToFrames().Take(10).Sum(f => f.Sum());
+			var game = EndGame();
+			return ConvertToTries(game).ToScoringGroups().Take(10).Sum(f => f.Sum());
+		}
+
+		private IEnumerable<Frame> EndGame() {
+			foreach (var frame in results)
+				yield return frame;
+
+			int frames = results.Count;
+			while (frames < 11) {
+				yield return Frame.Empty;
+			}
+		}
+
+		private IEnumerable<int> ConvertToTries(IEnumerable<Frame> frames) {
+			return frames.SelectMany(f => f.Tries());
 		}
 	}
 
-	public static class BlowingFrameExtensions {
-		public static IEnumerable<IEnumerable<int>> ToFrames(this IEnumerable<int> list) {
+	class Frame
+	{
+		public static Frame Empty = new Frame(0, 0);
+
+		List<int> results = new List<int>();
+
+		public Frame(params int[] tries) {
+			results.AddRange(tries);
+		}
+
+		public IEnumerable<int> Tries() {
+			return results.AsEnumerable();
+		}
+	}
+
+	public static class BowlingFrameExtensions {
+		public static IEnumerable<IEnumerable<int>> ToScoringGroups(this IEnumerable<int> list) {
 			yield return list.TakeFrame();
 
-			foreach (var pair in list.SkipFrame().ToFrames()) {
+			foreach (var pair in list.SkipFrame().ToScoringGroups()) {
 				yield return pair;
 			}
 		}
@@ -37,7 +71,7 @@ namespace Sandbox
 				return 3;
 			return 2;
 		}
-			
+
 		public static IEnumerable<int> SkipFrame(this IEnumerable<int> list) {
 			return list.Skip(list.NumberToSkip());
 		}
