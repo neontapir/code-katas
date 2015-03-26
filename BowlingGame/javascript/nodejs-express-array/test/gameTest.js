db22a1ffe05316d31a70e8f52ac0107d7c882a65
Chuck Durfee <chuck@durfweb.com>
2015-03-26 14:44:09 -0600

    array-based implementation

--- BowlingGame/javascript/nodejs-express-array/test/gameTest.js
+++ BowlingGame/javascript/nodejs-express-array/test/gameTest.js
@@ -4,10 +4,42 @@ var game = require('../game.js').app;
 
 describe('Scoring a bowling game', function() {
 	describe('a gutter game', function() {
-		it('should return 0', function() {
-			request(game).get('/score').expect(200).end(function(err,res) {
-				res.body.should.eql('0');
-			});
+		var expectedValue = '0';
+		it('should return ' + expectedValue, function(done) {
+			request(game).get('/score/0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0')
+			.expect(expectedValue, done);
+		});
+	});
+		
+	describe('a single pin game', function() {
+		var expectedValue = '20';
+		it('should return ' + expectedValue, function(done) {
+			request(game).get('/score/1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1')
+			.expect(expectedValue, done);
+		});
+	});
+	
+	describe('a single spare game', function() {
+		var expectedValue = '18';
+		it('should return ' + expectedValue, function(done) {
+			request(game).get('/score/7,3,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0')
+			.expect(expectedValue, done);
+		});
+	});
+	
+	describe('a single strike game', function() {
+		var expectedValue = '24';
+		it('should return ' + expectedValue, function(done) {
+			request(game).get('/score/10,7,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0')
+			.expect(expectedValue, done);
+		});
+	});
+	
+	describe('a perfect game', function() {
+		var expectedValue = '300';
+		it('should return ' + expectedValue, function(done) {
+			request(game).get('/score/10,10,10,10,10,10,10,10,10,10,10,10')
+			.expect(expectedValue, done);
 		});
 	});
 });
