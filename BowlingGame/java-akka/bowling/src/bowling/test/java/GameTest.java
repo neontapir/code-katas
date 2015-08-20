40ce3061f97d9019c260cd0e8456cbadb8fc838a
Chuck Durfee <charles.durfee@sap.com>
2015-08-19 18:04:55 -0600

    first commit

---
+++ BowlingGame/java-akka/bowling/src/bowling/test/java/GameTest.java
@@ -0,0 +1,12 @@
+package bowling.test.java;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class GameTest {
+    @Test
+    public void gutterGameTest() {
+        assertTrue(false);
+    }
+}
