5f916b329fdfd44e21719bc8180f7cfd89b6c485
Chuck Durfee <neontapir@gmail.com>
2018-04-11 11:49:30 -0600

    refactor update_quality to reduce duplication

--- ruby/lib/gilded_rose.rb
+++ ruby/lib/gilded_rose.rb
@@ -5,53 +5,36 @@ class GildedRose
     @items = items
   end
 
-  def update_quality()
+  def update_quality
     @items.each do |item|
-      if item.name != "Aged Brie" && item.name != "Backstage passes to a TAFKAL80ETC concert"
-        if item.quality > 0
-          if item.name != "Sulfuras, Hand of Ragnaros"
-            item.quality = item.quality - 1
-          end
-        end
-      else
-        if item.quality < 50
-          item.quality = item.quality + 1
-          if item.name == "Backstage passes to a TAFKAL80ETC concert"
-            if item.sell_in < 11
-              if item.quality < 50
-                item.quality = item.quality + 1
-              end
-            end
-            if item.sell_in < 6
-              if item.quality < 50
-                item.quality = item.quality + 1
-              end
-            end
-          end
-        end
-      end
-      if item.name != "Sulfuras, Hand of Ragnaros"
-        item.sell_in = item.sell_in - 1
-      end
-      if item.sell_in < 0
-        if item.name != "Aged Brie"
-          if item.name != "Backstage passes to a TAFKAL80ETC concert"
-            if item.quality > 0
-              if item.name != "Sulfuras, Hand of Ragnaros"
-                item.quality = item.quality - 1
-              end
-            end
-          else
-            item.quality = item.quality - item.quality
-          end
-        else
-          if item.quality < 50
-            item.quality = item.quality + 1
-          end
-        end
-      end
+      next if item.name == 'Sulfuras, Hand of Ragnaros'
+      update_item_quality(item) unless item.sell_in.negative?
+      age_item(item)
+      update_item_quality(item) if item.sell_in.negative?
     end
   end
+
+  def update_item_quality(item)
+    item.quality =
+      if item.name == 'Backstage passes to a TAFKAL80ETC concert'
+        if item.sell_in.negative?
+          0
+        else
+          extra = case item.sell_in
+                  when 6..10 then 2
+                  when 0..5 then 3
+                  else 1
+                  end
+          item.quality + extra
+        end
+      elsif item.name == 'Aged Brie'
+        [50, item.quality + 1].min
+      else
+        [0, item.quality - 1].max
+      end
+  end
+
+  def age_item(item)
+    item.sell_in -= 1
+  end
 end
-
-
