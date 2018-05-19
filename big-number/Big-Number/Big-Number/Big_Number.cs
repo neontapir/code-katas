3d92d474ac314d988e6a2a10dadd7779e4128f39
Chuck Durfee <chuck@durfweb.com>
2018-05-19 08:40:52 -0600

    add two numbers as string representation

---
+++ big-number/Big-Number/Big-Number/Big_Number.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Big_Number
+{
+    public class Big_Number
+    {
+        private char[] value;
+
+        public Big_Number(IEnumerable<char> v)
+        {
+            value = v.ToArray();
+        }
+
+		public static Big_Number operator +(Big_Number one, Big_Number another)
+		{
+			return one.Add(another);
+		}
+
+		private Big_Number Add(Big_Number another)
+		{
+			var me = ToString();
+			var other = another.ToString();
+			int length = Math.Max(me.Length, other.Length);
+			me = me.PadLeft(length, '0');
+			other = other.PadLeft(length, '0');
+
+			var sb = new StringBuilder();
+			int carry = 0;
+			for (int i = length - 1; i > -1; i--)
+			{
+				carry = ComputeDigitSum(me, other, sb, carry, i);
+			}
+			return new Big_Number(sb.ToString().Reverse().ToArray());
+		}
+
+		private int ComputeDigitSum(string me, string other, StringBuilder sb, int carry, int index)
+		{
+			int x = GetDigit(me, index);
+			int y = GetDigit(other, index);
+			int sum = x + y + carry;
+			if (sum >= 10)
+			{
+				carry = 1;
+				sum -= 10;
+			}
+			else
+			{
+				carry = 0;
+			}
+			sb.Append(sum);
+			return carry;
+		}
+
+		private int GetDigit(string input, int index)
+        {
+            return input.Length > index ? Int32.Parse(input[index].ToString()) : 0;
+        }
+
+        public override string ToString() => new String(value.ToArray());
+
+        public override bool Equals(object obj)
+        {
+            return (obj is Big_Number) && (ToString() == obj.ToString());
+        }
+
+        public override int GetHashCode() => ToString().GetHashCode();
+    }
+}
---
+++ big-number/Big-Number/Big-Number/Tests/Addition.cs
@@ -0,0 +1,47 @@
+using Xunit;
+
+namespace Big_Number
+{
+	public class Addition
+	{
+		[Fact]
+		public void Can_add_two_numbers_without_carrying()
+		{
+			Big_Number x = new Big_Number("2");
+			Big_Number y = new Big_Number("7");
+			Assert.Equal(new Big_Number("9"), x + y);
+		}
+
+		[Fact]
+        public void Can_add_two_numbers_of_different_sizes_without_carrying()
+        {
+            Big_Number x = new Big_Number("12");
+            Big_Number y = new Big_Number("6");
+			Assert.Equal(new Big_Number("18"), x + y);
+        }
+
+		[Fact]
+		public void Can_add_two_multiple_digit_numbers_without_carrying()
+		{
+			Big_Number x = new Big_Number("13");
+			Big_Number y = new Big_Number("26");
+			Assert.Equal(new Big_Number("39"), x + y);
+		}
+
+		[Fact]
+        public void Can_add_two_numbers_with_carrying()
+        {
+            Big_Number x = new Big_Number("18");
+            Big_Number y = new Big_Number("72");
+			Assert.Equal(new Big_Number("90"), x + y);
+        }
+
+		[Fact]
+        public void Can_add_two_big_numbers()
+        {
+			Big_Number x = new Big_Number("1234567890123456789012345678901234567890");
+			Big_Number y = new Big_Number("1234567890123456789012345678901234567890");
+			Assert.Equal(new Big_Number("2469135780246913578024691357802469135780"), x + y);
+        }
+    }
+}
