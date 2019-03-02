b7077208af5f98a118b2e4b0f58ca98a66d63af9
Chuck Durfee <neontapir@gmail.com>
2019-03-02 14:09:04 -0700

    remove tests from spec

--- commit-derby/test/game_spec.rb
+++ commit-derby/test/game_spec.rb
@@ -1,12 +1,7 @@
 require 'minitest/spec'
 require 'minitest/autorun'
+require_relative '../Game'
 
-describe Array do
-  it "can be created with no arguments" do
-    Array.new.must_be_instance_of Array
-  end
+describe Game do
 
-  it "can be created with a specific size" do
-    Array.new(10).size.must_equal 10
-  end
 end
