ee54651fd6387bc519e5810af5f5df3541e31f8f
Chuck Durfee <neontapir@gmail.com>
2022-12-13 14:05:53 -0700

    remove spec classes

--- advent_of_code_2022/day_03/03_rucksacks_spec.rb
+++ advent_of_code_2022/day_03/03_rucksacks_spec.rb
@@ -3,87 +3,85 @@ require 'rspec'
 require_relative('lib/rucksack.rb')
 require_relative('lib/rucksack_set.rb')
 
-class RucksackSpec
-  describe Rucksack do
-    context 'sample rucksack' do
-      subject(:sample) { Rucksack.new("vJrwpWtwJgWrhcsFMMfFFhFp") }
+describe Rucksack do
+  context 'sample rucksack' do
+    subject(:sample) { Rucksack.new("vJrwpWtwJgWrhcsFMMfFFhFp") }
 
-      it 'can divide a rucksack into two equal compartments' do
-        expect(sample.compartments).to eq ["vJrwpWtwJgWr".chars,"hcsFMMfFFhFp".chars]
-      end
-
-      it 'can find common elements in both compartments' do
-        expect(sample.in_both_compartments).to eq ["p"]
-      end
-
-      it 'can determine the priority of an item' do
-        expect(Rucksack.priority('p')).to eq 16
-        expect(sample.priorities_sum).to eq 16
-      end
+    it 'can divide a rucksack into two equal compartments' do
+      expect(sample.compartments).to eq ["vJrwpWtwJgWr".chars,"hcsFMMfFFhFp".chars]
     end
 
-    context 'second sample rucksack' do
-      subject(:sample) { Rucksack.new("jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL") }
-
-      it 'can find common elements in both compartments' do
-        expect(sample.in_both_compartments).to eq ["L"]
-      end
-
-      it 'can determine the priority of an item' do
-        expect(Rucksack.priority('L')).to eq 38
-      end
+    it 'can find common elements in both compartments' do
+      expect(sample.in_both_compartments).to eq ["p"]
     end
 
-    context 'part 1 sample set' do
-      subject(:rucksacks) { RucksackSet.new(File.readlines("day_03/day_03_sample.txt")) }
+    it 'can determine the priority of an item' do
+      expect(Rucksack.priority('p')).to eq 16
+      expect(sample.priorities_sum).to eq 16
+    end
+  end
 
-      it 'hydrates each rucksack correctly' do
-        expect(rucksacks.first).to eq Rucksack.new("vJrwpWtwJgWrhcsFMMfFFhFp")
-      end
+  context 'second sample rucksack' do
+    subject(:sample) { Rucksack.new("jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL") }
 
-      it 'can determine the priorities of each rucksack' do
-        expect(rucksacks.map{|r| r.in_both_compartments}).to eq [["p"], ["L"], ["P"], ["v"], ["t"], ["s"]]
-        expect(rucksacks.map{|r| r.priorities_sum}).to eq [16, 38, 42, 22, 20, 19]
-      end
-
-      it 'can determine the sum of priorities of a group' do
-        expect(rucksacks.priorities_total).to eq 157
-      end
+    it 'can find common elements in both compartments' do
+      expect(sample.in_both_compartments).to eq ["L"]
     end
 
-    context 'part 2 sample set' do
-      subject(:first_group) { ["vJrwpWtwJgWrhcsFMMfFFhFp",
-        "jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL",
-        "PmmdzqPrVvPwwTWBwg"] }
+    it 'can determine the priority of an item' do
+      expect(Rucksack.priority('L')).to eq 38
+    end
+  end
 
-      subject(:second_group) { ["wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn",
-        "ttgJtRGJQctTZtZT",
-        "CrZsJsPPZsGzwwsLwLmpwMDw"] }
-        
-      it 'can find common elements in all rucksacks' do
-        expect(RucksackSet.new(first_group).in_all_rucksacks).to eq ['r']
-        expect(RucksackSet.new(second_group).in_all_rucksacks).to eq ['Z']
-      end
+  context 'part 1 sample set' do
+    subject(:rucksacks) { RucksackSet.new(File.readlines("day_03/day_03_sample.txt")) }
 
-      it 'can split a set into triads' do
-        combined = first_group + second_group
-        actual = RucksackSet.new(combined).into_triads
-        expect(actual.first).to eq RucksackSet.new(first_group)
-        expect(actual.last).to eq RucksackSet.new(second_group)
-      end
+    it 'hydrates each rucksack correctly' do
+      expect(rucksacks.first).to eq Rucksack.new("vJrwpWtwJgWrhcsFMMfFFhFp")
     end
 
-    context 'input' do
-      subject(:rucksacks) { RucksackSet.new(File.readlines("day_03/day_03_input.txt")) }
+    it 'can determine the priorities of each rucksack' do
+      expect(rucksacks.map{|r| r.in_both_compartments}).to eq [["p"], ["L"], ["P"], ["v"], ["t"], ["s"]]
+      expect(rucksacks.map{|r| r.priorities_sum}).to eq [16, 38, 42, 22, 20, 19]
+    end
 
-      it 'can determine the sum of priorities of a group' do
-        expect(rucksacks.priorities_total).to eq 8240
-      end
+    it 'can determine the sum of priorities of a group' do
+      expect(rucksacks.priorities_total).to eq 157
+    end
+  end
 
-      it 'can determine the sum of priorities of item types' do
-        actual = rucksacks.into_triads.map{|t| t.badge_priority}.sum
-        expect(actual).to eq 2587
-      end
+  context 'part 2 sample set' do
+    subject(:first_group) { ["vJrwpWtwJgWrhcsFMMfFFhFp",
+      "jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL",
+      "PmmdzqPrVvPwwTWBwg"] }
+
+    subject(:second_group) { ["wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn",
+      "ttgJtRGJQctTZtZT",
+      "CrZsJsPPZsGzwwsLwLmpwMDw"] }
+      
+    it 'can find common elements in all rucksacks' do
+      expect(RucksackSet.new(first_group).in_all_rucksacks).to eq ['r']
+      expect(RucksackSet.new(second_group).in_all_rucksacks).to eq ['Z']
+    end
+
+    it 'can split a set into triads' do
+      combined = first_group + second_group
+      actual = RucksackSet.new(combined).into_triads
+      expect(actual.first).to eq RucksackSet.new(first_group)
+      expect(actual.last).to eq RucksackSet.new(second_group)
+    end
+  end
+
+  context 'input' do
+    subject(:rucksacks) { RucksackSet.new(File.readlines("day_03/day_03_input.txt")) }
+
+    it 'can determine the sum of priorities of a group' do
+      expect(rucksacks.priorities_total).to eq 8240
+    end
+
+    it 'can determine the sum of priorities of item types' do
+      actual = rucksacks.into_triads.map{|t| t.badge_priority}.sum
+      expect(actual).to eq 2587
     end
   end
 end
--- advent_of_code_2022/day_04/04_space_spec.rb
+++ advent_of_code_2022/day_04/04_space_spec.rb
@@ -2,50 +2,48 @@ require 'rspec'
 require_relative 'lib/space'
 require_relative 'lib/space_set'
 
-class SpaceSpec  
-  describe Space do
-    it 'can parse an assigment' do
-      expect(Space.new('2-4,6-8').assignments).to eq '2-4,6-8'
-      expect(Space.new('2-4,6-8').ranges).to eq [2..4, 6..8]
-    end
-
-    it 'can detect a full overlap' do
-      expect(Space.new('2-4,6-8').full_overlap?).to be_falsey
-      expect(Space.new('6-6,6-8').full_overlap?).to be_truthy
-    end
-
-    it 'can detect a partial overlap' do
-      expect(Space.new('5-6,6-8').partial_overlap?).to be_truthy
-      expect(Space.new('6-6,6-8').partial_overlap?).to be_truthy
-      expect(Space.new('2-5,6-8').partial_overlap?).to be_falsey
-    end
+describe Space do
+  it 'can parse an assigment' do
+    expect(Space.new('2-4,6-8').assignments).to eq '2-4,6-8'
+    expect(Space.new('2-4,6-8').ranges).to eq [2..4, 6..8]
   end
 
-  context 'part 1 sample set' do
-    subject(:assignmentPairs) { SpaceSet.new(File.readlines("day_04/day_04_sample.txt")) }
-
-    it 'can hydrate pairings' do
-      expect(assignmentPairs.first).to eq Space.new('2-4,6-8')
-    end
-
-    it 'can find all full overlaps' do
-      expect(assignmentPairs.filter{|p| p.full_overlap?}.map{|p| p.assignments}).to eq ["2-8,3-7", "6-6,4-6"]
-    end
+  it 'can detect a full overlap' do
+    expect(Space.new('2-4,6-8').full_overlap?).to be_falsey
+    expect(Space.new('6-6,6-8').full_overlap?).to be_truthy
   end
 
-  context 'part 1 input set' do
-    subject(:assignmentPairs) { SpaceSet.new(File.readlines("day_04/day_04_input.txt")) }
+  it 'can detect a partial overlap' do
+    expect(Space.new('5-6,6-8').partial_overlap?).to be_truthy
+    expect(Space.new('6-6,6-8').partial_overlap?).to be_truthy
+    expect(Space.new('2-5,6-8').partial_overlap?).to be_falsey
+  end
+end
 
-    it 'can count full overlaps' do
-      expect(assignmentPairs.full_overlaps.count).to eq 498
-    end
+context 'part 1 sample set' do
+  subject(:assignmentPairs) { SpaceSet.new(File.readlines("day_04/day_04_sample.txt")) }
+
+  it 'can hydrate pairings' do
+    expect(assignmentPairs.first).to eq Space.new('2-4,6-8')
   end
 
-  context 'part 2 input set' do
-    subject(:assignmentPairs) { SpaceSet.new(File.readlines("day_04/day_04_input.txt")) }
+  it 'can find all full overlaps' do
+    expect(assignmentPairs.filter{|p| p.full_overlap?}.map{|p| p.assignments}).to eq ["2-8,3-7", "6-6,4-6"]
+  end
+end
 
-    it 'can count partial overlaps' do
-      expect(assignmentPairs.partial_overlaps.count).to eq 859
-    end
+context 'part 1 input set' do
+  subject(:assignmentPairs) { SpaceSet.new(File.readlines("day_04/day_04_input.txt")) }
+
+  it 'can count full overlaps' do
+    expect(assignmentPairs.full_overlaps.count).to eq 498
+  end
+end
+
+context 'part 2 input set' do
+  subject(:assignmentPairs) { SpaceSet.new(File.readlines("day_04/day_04_input.txt")) }
+
+  it 'can count partial overlaps' do
+    expect(assignmentPairs.partial_overlaps.count).to eq 859
   end
 end
