4fb47e50a4cf45c2c16c2fb7dd69697fa2fbc840
Chuck Durfee <neontapir@gmail.com>
2024-06-25 21:35:00 -0600

    refactor to achieve my initial concept

--- diamond-kata/src/diamond.py
+++ diamond-kata/src/diamond.py
@@ -5,24 +5,40 @@ class Diamond:
         self.spec: str = spec
         self.diamond_array: list[str] = self._create()
 
+    # NOTE: The idea behind _create is to create a block of strings that
+    #   contains the shape I want.
+    # For example, for a spec of "C", I'd start with:
+    #   CBABC
+    #   CBABC
+    #   CBABC
+    #   CBABC
+    #   CBABC
+    # Then, if I mask all but A,B,C,B,A with spaces, I get:
+    #     A
+    #    B B
+    #   C   C
+    #    B B
+    #     A
+    # The first step is accomplished by letters; the second, by make_line.
+    # However, we have to erase the letters in the opposite order as the
+    #   generated line in letters, hence masking_sequence.
+
     def _create(self) -> list[str]:
-        if self.spec == "A":
-            return ["A"]
+        def mirrored_seq(list: list[str]) -> list[str]:
+            """from 'CBA' to 'CBABC'"""
+            return list + list[0:-1][::-1]
 
-        letters: list[str] = [chr(i) for i in range(ord("A"), ord(self.spec) + 1)]
-        width: int = 2 * len(letters) - 1
+        # with self.spec = 'C', template is 'CBA'
+        template: list[str] = [chr(i) for i in range(ord(self.spec), ord("A") - 1, -1)]
+        diamond_template: list[str] = mirrored_seq(template)
 
-        def make_line(i, letter) -> str:
-            if i == 0:  # For 'A'
-                return letter.center(width)
-            outer_spaces: int = width // 2 - i
-            inner_spaces: int = width - 2 * outer_spaces - 2
-            return f"{' ' * outer_spaces}{letter}{' ' * inner_spaces}{letter}{' ' * outer_spaces}"
+        unmasked_letters = list(reversed(template))
+        masking_sequence = mirrored_seq(unmasked_letters)
 
-        top_half: list[str] = [make_line(i, letter) for i, letter in enumerate(letters)]
-        bottom_half: list[str] = top_half[-2::-1]
+        def make_line(letter):
+            return "".join(" " if c != letter else c for c in diamond_template)
 
-        return top_half + bottom_half
+        return [make_line(x) for x in masking_sequence]
 
     def print(self):
         print("\n".join(self.diamond_array))
